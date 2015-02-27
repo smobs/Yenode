19cec7fdcd98b9c545855a69d42944abd4facc0c
Toby Smyth <tobs169@gmail.com>
2015-02-27 01:15:11 +0000

    inherit sickness

--- hdevenv.nix
+++ hdevenv.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }: with pkgs;
-
+let
+  inherit (haskellPackages_ghc783_profiling) cabal2nix yesodBin;
+in
 {
   environment.systemPackages = [
     ghc.ghc783
-    haskellPackages_ghc783_profiling.cabal2nix
-    haskellPackages_ghc783_profiling.yesodBin
+    cabal2nix
+    yesodBin
   ];
 }
